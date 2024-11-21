d5a2a60bef89cc836c401085a969b7f10aa21f8c
8anooceu <jaocarrilhooo2903@gmail.com>
2024-11-21 14:47:50 -0300

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,9 +1,4 @@
-// Exibir a tela de carregamento e remover após 3 segundos
-window.addEventListener("load", () => {
-    const loadingScreen = document.getElementById("loading-screen");
-    setTimeout(() => {
-        loadingScreen.style.display = "none";
-    }, 3000);
+
 });
 
 // Retornar para a página anterior ao clicar no botão
@@ -22,10 +17,6 @@ setTimeout(() => {
     message.textContent = "404 is out of orbit! Refresh the page to bring it back.";
     message.style.color = "#ff6f61"; // Muda a cor da mensagem para destaque
 }, 6000);
-// Mostra o conteúdo e inicia a animação do 404 após o carregamento
-setTimeout(() => {
-    document.querySelector(".loading").style.display = "none";
-    document.querySelector(".main-content").style.display = "block";
 
     // Inicia a animação do 404
     const astronaut = document.querySelector(".astronaut");
